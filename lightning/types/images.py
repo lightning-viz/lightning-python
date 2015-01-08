e49ebf7a297c108d3b9b0f9e56f1cd9d5bd338ed
freeman <the.freeman.lab@gmail.com>
2015-01-08 18:50:16 -0500

    Better docs and input handling for images

--- lightning/types/images.py
+++ lightning/types/images.py
@@ -1,3 +1,5 @@
+from numpy import ndarray, asarray
+
 from lightning.types.base import Base
 from lightning.types.decorators import viztype
 from lightning.types.utils import array_to_im
@@ -9,31 +11,27 @@ class Image(Base):
     
     @staticmethod
     def clean(imagedata):
+        """
+        Display an array as an image.
 
-        out = []
-        out.append(array_to_im(imagedata))
-        return {'images': out}
+        Parameters
+        ----------
+        imagedata : array-like
+            Image as a two dimensional (grayscale) or three dimensional (RGB) array.
+        """
+        if asarray(imagedata).ndim not in set((2, 3)):
+            raise Exception("Input must be two or three dimensional")
 
-    @property
+        outdict = [array_to_im(imagedata)]
+
+        return {'images': outdict}
+
     def coords(self):
         user_data = self.get_user_data()['settings']
         if 'coords' in user_data.keys():
             return user_data['coords']
         else:
             return []
-
-@viztype
-class Volume(Base):
-
-    _name = 'volume'
-
-    @staticmethod
-    def clean(imagedata):
-
-        out = []
-        for im in imagedata:
-            out.append(array_to_im(im))
-        return {'images': out}
 
 
 @viztype
@@ -43,8 +41,18 @@ class Gallery(Base):
 
     @staticmethod
     def clean(imagedata):
+        """
+        Display a collection of arrays as browsable images with thumbnails.
 
-        out = []
-        for im in imagedata:
-            out.append(array_to_im(im))
-        return {'images': out}
+        Parameters
+        ----------
+        imagedata : array-like, or list of array-like
+            Image or list of images as two dimensional (grayscale) or three dimensional (RGB) arrays.
+        """
+
+        if isinstance(imagedata, ndarray):
+            imagedata = [imagedata]
+
+        outdict = [array_to_im(im) for im in imagedata]
+
+        return {'images': outdict}
