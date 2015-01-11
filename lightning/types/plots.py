e97452833ec49d2bfbdb9f5f2e4edbb98ed78fe0
freeman <the.freeman.lab@gmail.com>
2015-01-11 15:21:00 -0500

    Move stacked line / line streaming

--- lightning/types/plots.py
+++ lightning/types/plots.py
@@ -52,45 +52,6 @@ class Scatter(Base):
 
 
 @viztype
-class ScatterStreaming(Base):
-
-    _name = 'scatter-streaming'
-    _func = 'scatterstreaming'
-
-    @staticmethod
-    def clean(x, y, color=None, label=None, size=None):
-        """
-        Create a streaming scatter plot of x and y.
-
-        Plotting once returns a visualization on which 'append' can be called to add new data
-        in a streaming fashion. The opacity of old and new data is automatically set
-        to highlight the most recent data and fade old data away.
-
-        Parameters
-        ----------
-        x, y : array-like, each (n,)
-            Input data
-
-        color : array-like, optional, singleton or (n,3)
-            Single rgb value or array to set colors
-
-        label : array-like, optional, singleton or (n,)
-            Single integer or array to set colors via groups
-
-        size : array-like, optional, singleton or (n,)
-            Single size or array to set point sizes
-        """
-        
-        points = vecs_to_points(x, y)
-        outdict = {'points': points}
-
-        outdict = add_property(outdict, color, 'color')
-        outdict = add_property(outdict, label, 'label')
-        outdict = add_property(outdict, size, 'size')
-
-        return outdict
-
-@viztype
 class ScatterLine(Base):
 
     _name = 'scatter-line'
@@ -230,6 +191,19 @@ class Line(Base):
         outdict = add_property(outdict, index, 'index')
 
         return outdict
+
+
+@viztype
+class LineStacked(Base):
+
+    _name = 'line-stacked'
+    _func = 'linestacked'
+
+    @staticmethod
+    def clean(series):
+
+        data = array_to_lines(series)
+        return {'data': data}
 
 
 @viztype
--- lightning/types/streaming.py
+++ lightning/types/streaming.py
@@ -15,14 +15,42 @@ class LineStreaming(Base):
         data = array_to_lines(series)
         return {'data': data}
 
-@viztype
-class LineStacked(Base):
 
-    _name = 'line-stacked'
-    _func = 'linestacked'
+@viztype
+class ScatterStreaming(Base):
+
+    _name = 'scatter-streaming'
+    _func = 'scatterstreaming'
 
     @staticmethod
-    def clean(series):
+    def clean(x, y, color=None, label=None, size=None):
+        """
+        Create a streaming scatter plot of x and y.
 
-        data = array_to_lines(series)
-        return {'data': data}
+        Plotting once returns a visualization on which 'append' can be called to add new data
+        in a streaming fashion. The opacity of old and new data is automatically set
+        to highlight the most recent data and fade old data away.
+
+        Parameters
+        ----------
+        x, y : array-like, each (n,)
+            Input data
+
+        color : array-like, optional, singleton or (n,3)
+            Single rgb value or array to set colors
+
+        label : array-like, optional, singleton or (n,)
+            Single integer or array to set colors via groups
+
+        size : array-like, optional, singleton or (n,)
+            Single size or array to set point sizes
+        """
+
+        points = vecs_to_points(x, y)
+        outdict = {'points': points}
+
+        outdict = add_property(outdict, color, 'color')
+        outdict = add_property(outdict, label, 'label')
+        outdict = add_property(outdict, size, 'size')
+
+        return outdict
