5a044683a90530ae17f20996708b9abb8f9e5270
Matthew Conlen <mc@mathisonian.com>
2015-01-11 14:30:33 -0500

    fix auth

--- lightning/visualization.py
+++ lightning/visualization.py
@@ -6,9 +6,10 @@ import urllib
 
 class Visualization(object):
 
-    def __init__(self, session=None, json=None):
+    def __init__(self, session=None, json=None, auth=None):
         self.session = session
         self.id = json.get('id')
+        self.auth = auth
 
     def _format_url(self, url):
         if not url.endswith('/'):
@@ -20,13 +21,13 @@ class Visualization(object):
         url = self.session.host + '/sessions/' + str(self.session.id) + '/visualizations/' + str(self.id) + '/data/images'
         url = self._format_url(url)
         files = {'file': image}
-        return requests.put(url, files=files, data={'type': 'image'})
+        return requests.put(url, files=files, data={'type': 'image'}, auth=self.auth)
 
     def append_image(self, image):
         url = self.session.host + '/sessions/' + str(self.session.id) + '/visualizations/' + str(self.id) + '/data/images'
         url = self._format_url(url)
         files = {'file': image}
-        return requests.post(url, files=files, data={'type': 'image'})
+        return requests.post(url, files=files, data={'type': 'image'}, auth=self.auth)
 
     def append_data(self, data=None, field=None):
         payload = {'data': data}
@@ -36,7 +37,7 @@ class Visualization(object):
             url += field
 
         url = self._format_url(url)
-        return requests.post(url, data=json.dumps(payload), headers=headers)
+        return requests.post(url, data=json.dumps(payload), headers=headers, auth=self.auth)
 
     def update_data(self, data=None, field=None):
         payload = {'data': data}
@@ -46,7 +47,7 @@ class Visualization(object):
             url += field
 
         url = self._format_url(url)
-        return requests.put(url, data=json.dumps(payload), headers=headers)
+        return requests.put(url, data=json.dumps(payload), headers=headers, auth=self.auth)
 
     def get_permalink(self):
         return self.session.host + '/visualizations/' + str(self.id)
@@ -55,11 +56,9 @@ class Visualization(object):
         return self._format_url(self.get_permalink() + '/embed')
 
     def get_html(self):
-        import urllib2
 
-        response = urllib2.urlopen(self.get_embed_link())
-
-        return response.read()
+        r = requests.get(self.get_embed_link(), auth=self.auth)
+        return r.text
 
     def open(self):
         webbrowser.open(self.session.host + '/visualizations/' + str(self.id) + '/')
@@ -70,7 +69,7 @@ class Visualization(object):
 
 
     @classmethod
-    def create(cls, session=None, data=None, images=None, type=None, auth=None):
+    def create(cls, session=None, data=None, images=None, type=None):
         url = session.host + '/sessions/' + str(session.id) + '/visualizations'
 
         if not images:
@@ -82,7 +81,7 @@ class Visualization(object):
             if not r.status_code == requests.codes.ok:
                 raise Exception('Problem uploading data')
 
-            viz = cls(session=session, json=r.json())
+            viz = cls(session=session, json=r.json(), auth=session.auth)
 
         else:
 
@@ -94,7 +93,7 @@ class Visualization(object):
             if not r.status_code == requests.codes.ok:
                 raise Exception('Problem uploading images')
 
-            viz = cls(session=session, json=r.json())
+            viz = cls(session=session, json=r.json(), auth=session.auth)
             for image in remaining_images:
                 viz.append_image(image)
 
