617b4157b6f47a82172b3f18d9e6c3ed3b74adaa
freeman <the.freeman.lab@gmail.com>
2015-01-12 03:19:17 -0500

    Add docs

--- lightning/main.py
+++ lightning/main.py
@@ -16,14 +16,22 @@ class Lightning(object):
             self.enable_ipython()
 
     def __repr__(self):
-        s = 'Lightning at host: %s' % self.host
-        return s
+        if hasattr(self, 'session') and self.session is not None:
+            return 'Lightning server at host: %s' % self.host + '\n' + self.session.__repr__()
+        else:
+            return 'Lightning server at host: %s' % self.host
 
     def enable_ipython(self, **kwargs):
         """
-        ipython code inspired by code powering similar functionality in mpld3:
-        https://github.com/jakevdp/mpld3/blob/master/mpld3/_display.py#L357
+        Enable plotting in the iPython notebook.
+
+        Once enabled, all lightning plots will be automatically produced
+        within the iPython notebook. They will also be available on
+        your lightning server within the current session.
         """
+
+        # inspired by code powering similar functionality in mpld3
+        # https://github.com/jakevdp/mpld3/blob/master/mpld3/_display.py#L357
 
         from IPython.core.getipython import get_ipython
         ip = get_ipython()
@@ -32,29 +40,65 @@ class Lightning(object):
                            lambda viz, kwds=kwargs: viz.get_html())
 
     def disable_ipython(self):
+        """
+        Disable plotting in the iPython notebook.
+
+        After disabling, lightning plots will be produced in your lightning server,
+        but will not appear in the notebook.
+        """
         from IPython.core.getipython import get_ipython
         ip = get_ipython()
         formatter = ip.display_formatter.formatters['text/html']
         formatter.type_printers.pop(Visualization, None)
 
     def create_session(self, name=None):
+        """
+        Create a lightning session.
+
+        Can create a session with the provided name, otherwise session name
+        will be "Session No." with the number automatically generated.
+        """
         self.session = Session.create(self.host, name=name, auth=self.auth)
         return self.session
 
     def use_session(self, session_id):
+        """
+        Use the specified lightning session.
+
+        Specify a lightning session by id number. Check the number of an existing
+        session in the attribute lightning.session.id.
+        """
         self.session = Session(host=self.host, id=session_id, auth=self.auth)
         return self.session
 
     def set_basic_auth(self, username, password):
+        """
+        Set authenatication.
+        """
         from requests.auth import HTTPBasicAuth
         self.auth = HTTPBasicAuth(username, password)
         return self
 
     def set_host(self, host):
+        """
+        Set the host for a lightning server.
+
+        Host can be local (e.g. http://localhost:3000), a heroku
+        instance (e.g. http://lightning-test.herokuapp.com), or
+        a independently hosted lightning server.
+        """
         self.host = host
         return self
 
     def plot(self, type=None, **kwargs):
+        """
+        Generic plotting function.
+
+        Provide an arbtirary data object as a dictionary,
+        and a plot type as a string. Can be used to provide data to custom
+        plot types, as opposed to the included plot types
+        (e.g. lightning.scatter, lightning.line, etc.)
+        """
 
         from types.plots import Generic
         
