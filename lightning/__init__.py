1ceddde5c3ab761a01f63f355130ea055ec42ac2
freeman <the.freeman.lab@gmail.com>
2015-01-12 03:47:27 -0500

    First commit of doc pages

--- lightning/__init__.py
+++
@@ -1,9 +0,0 @@
-from .main import Lightning
-from .session import Session
-from .visualization import Visualization
-from .types.plots import *
-from .types.images import *
-from .types.streaming import *
-from .types.three import *
-
-__version__ = "0.3.0"
