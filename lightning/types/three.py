586aec30f4bfbd0748584cdc9c143716e67b2f0d
Matthew Conlen <mc@mathisonian.com>
2015-01-15 16:45:16 -0500

    add size / alpha to particles

--- lightning/types/three.py
+++ lightning/types/three.py
@@ -8,7 +8,7 @@ class Particles(Base):
     _name = 'particles'
 
     @staticmethod
-    def clean(x, y, z, color=None, label=None):
+    def clean(x, y, z, color=None, label=None, alpha=None, size=None):
         """
         Plot three-dimensional data as points.
 
@@ -21,6 +21,16 @@ class Particles(Base):
 
         color : array-like, optional, singleton or (n,3)
             Single rgb value or array to set colors
+
+        label : array-like, optional, singleton or (n,)
+            Single integer or array to set colors via groups
+
+        size : array-like, optional, singleton or (n,)
+            Single size or array to set point sizes
+
+        alpha : array-like, optional, singleton or (n,)
+            Single alpha value or array to set fill and stroke opacity
+
         """
 
         points = vecs_to_points_three(x, y, z)
@@ -28,5 +38,7 @@ class Particles(Base):
 
         outdict = add_property(outdict, color, 'color')
         outdict = add_property(outdict, label, 'label')
+        outdict = add_property(outdict, size, 'size')
+        outdict = add_property(outdict, alpha, 'alpha')
 
         return outdict
