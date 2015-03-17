0ba22ef1fc99ebeb302aadd2d44f63c0a67c6c3a
freeman <the.freeman.lab@gmail.com>
2015-03-17 01:08:19 -0400

    Add imagedraw method

--- lightning/types/images.py
+++ lightning/types/images.py
@@ -8,6 +8,30 @@ from lightning.types.utils import array_to_im, polygon_to_points, polygon_to_mas
 @viztype
 class Image(Base):
     _name = 'image'
+
+    @staticmethod
+    def clean(imagedata):
+        """
+        Display an array as an image.
+
+        .. image:: image.png
+
+        Parameters
+        ----------
+        imagedata : array-like
+            Image as a two dimensional (grayscale) or three dimensional (RGB) array.
+        """
+        if asarray(imagedata).ndim not in set((2, 3)):
+            raise Exception("Input must be two or three dimensional")
+
+        outdict = [array_to_im(imagedata)]
+
+        return {'images': outdict}
+
+@viztype
+class ImageDraw(Base):
+    _name = 'image-draw'
+    _func = 'imagedraw'
     
     @staticmethod
     def clean(imagedata):
