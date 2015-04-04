39f157d8562f1bf2e236a3177c871c8b0f1c099f
freeman <the.freeman.lab@gmail.com>
2015-04-03 22:18:31 -0400

    Add favicon

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -121,6 +121,9 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'bootstrap'
 
+html_favicon = "favicon.ico"
+
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
